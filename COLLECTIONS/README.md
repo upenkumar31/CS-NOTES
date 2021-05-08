189308101bf83982ebe43c7ed57171e4e4850467
unishad <unishad@rbbn.com>
2021-05-08 09:39:05 +0530

    ArrayList

--- COLLECTIONS/README.md
+++ COLLECTIONS/README.md
@@ -1,1 +1,11 @@
-1 ![img.png](img.png)
+# **ArrayList**
+1. Java ArrayList class uses a dynamic array for storing the elements
+2. It is like an array, but there is no size limit
+3. It implements the List interface, so we can use all the methods of List interface here
+4. Java ArrayList class can contain duplicate elements.
+5.  Java ArrayList class maintains insertion order.
+6.  Java ArrayList class is non synchronized.
+7.  Java ArrayList allows random access because array works at the index basis.
+8. In ArrayList, manipulation is little slower than the LinkedList in Java because a lot of shifting needs to occur if any element is removed from the array list.
+
+`public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable`
---
+++ COLLECTIONS/src/ArrayListExample.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayListExample {
+    ArrayList<Integer> arrayList;
+
+    void Example() {
+        arrayList = new ArrayList<>();
+
+        //Adding Element
+        arrayList.add(12);
+        arrayList.add(1, 4); // index,value
+        arrayList.addAll(new LinkedList<>()); // adding element from other collection
+        arrayList.addAll(5, new LinkedList<>()); // adding from certain index
+        arrayList.set(2, 12);
+
+        //Deleting Element
+        arrayList.remove(new Integer(12)); // Deleting Object
+        arrayList.remove(1);
+        arrayList.removeAll(new LinkedList<>()); // remove all value present in linkedList
+        arrayList.removeIf(a -> a > 12); // removing element if element is greater that 12 (used Lambda)
+        arrayList.clear();
+
+        //Cloning
+        ArrayList<Integer> cloneArrayList = (ArrayList<Integer>) arrayList.clone();
+
+        //Searching
+        arrayList.indexOf(new Integer(2));
+        arrayList.contains(new Integer(12));
+
+        //Iterating
+        arrayList.forEach(a -> System.out.print(a));
+        for (int i = 0; i < arrayList.size(); i++) {
+        }
+
+        //Capacity Increasing
+        arrayList.ensureCapacity(20);
+
+        //get element at index
+        arrayList.get(2);
+
+        //checking if empty or not
+        arrayList.isEmpty();
+
+        //Size Trimming
+        arrayList.trimToSize();
+
+        //getting sublist
+        arrayList.subList(2,7)
+    }
+}
